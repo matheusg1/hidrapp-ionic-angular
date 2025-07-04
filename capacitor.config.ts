98e91513ca396eb9072be205a26c85c96bf59950
unknown <tecmatheusg@hotmail.com>
2025-07-04 19:54:36 -0300

    feat: add capacitor android

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'hidrapp',
+  appId: 'com.matheusg1.hidratapp',
+  appName: 'hidratapp',
   webDir: 'www'
 };
 
